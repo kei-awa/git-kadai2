4aa20591ee786a28bbd4ec1486e563e8aeccc062
kei.awa <ogita2siam.git@gmail.com>
2020-02-09 04:32:56 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h2>おみくじ</h2>
+        <p>おみくじ結果:無し</p>
+        <form action ="sample.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじをひく!</button>
+        </form>
+    </body>
+</html>
